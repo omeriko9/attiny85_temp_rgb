e639fb7bc05a6c1a9a9ade32543876c599f30f63
omeriko9 <omeriko9@gmail.com>
2023-09-10 23:54:11 +0300

    updated code

--- revise_attiny_RGB_temperature.ino
+++ revise_attiny_RGB_temperature.ino
@@ -13,91 +13,41 @@
 #define SETTLE 1000
 #define FADE_TIME 2
 
-SoftwareSerial mySerial(PB3,PB2);
+SoftwareSerial mySerial(PB3, PB2);
 OneWire oneWire(TEMP_PIN);
 DallasTemperature sensor(&oneWire);
 
 
 uint8_t currentR = 0, currentG = 0, currentB = 0;
+char buf[45];
 
-// Common code for both sources of an ADC conversion
-int getADC() {
-  ADCSRA  |=_BV(ADSC);           // Start conversion
-  while((ADCSRA & _BV(ADSC)));    // Wait until conversion is finished
-  return ADC;
-}
 
-float chipTemp(float raw) {
-  const float chipTempOffset = 272.9;           // Your value here, it may vary 
-  const float chipTempCoeff = 1.075;            // Your value here, it may vary
-  return((raw - chipTempOffset) / chipTempCoeff);
-}
-
-int getChipTemperatureCelsius() {
-  int i;
-  int t_celsius; 
-  uint8_t vccIndex;
-  float rawTemp, rawVcc;
-  
-  // Measure temperature
-  ADCSRA |= _BV(ADEN);           // Enable AD and start conversion
-  ADMUX = 0xF | _BV( REFS1 );    // ADC4 (Temp Sensor) and Ref voltage = 1.1V;
-  delay(SETTLE);                 // Settling time min 1 ms, wait 100 ms
-
-  rawTemp = (float)getADC();     // use next sample as initial average
-  for (int i=2; i<2000; i++) {   // calculate running average for 2000 measurements
-    rawTemp += ((float)getADC() - rawTemp) / float(i); 
-  }  
-  ADCSRA &= ~(_BV(ADEN));        // disable ADC  
-
-  // Measure chip voltage (Vcc)
-  ADCSRA |= _BV(ADEN);   // Enable ADC
-  ADMUX  = 0x0c | _BV(REFS2);    // Use Vcc as voltage reference, 
-                                 //    bandgap reference as ADC input
-  delay(SETTLE);                    // Settling time min 1 ms, there is 
-                                 //    time so wait 100 ms
-  rawVcc = (float)getADC();      // use next sample as initial average
-  for (int i=2; i<2000; i++) {   // calculate running average for 2000 measurements
-    rawVcc += ((float)getADC() - rawVcc) / float(i);
-  }
-  ADCSRA &= ~(_BV(ADEN));        // disable ADC
-  
-  rawVcc = 1024 * 1.1f / rawVcc;
-  //index 0..13 for vcc 1.7 ... 3.0
-  vccIndex = min(max(17,(uint8_t)(rawVcc * 10)),30) - 17;   
-
-  // Temperature compensation using the chip voltage 
-  // with 3.0 V VCC is 1 lower than measured with 1.7 V VCC 
-  t_celsius = (int)(chipTemp(rawTemp) + (float)vccIndex / 13);  
-                                                                                   
-  return t_celsius;
-}
-
-void reset_color(){
+void reset_color() {
   //reset
-   digitalWrite(RED_PIN,0);
-   digitalWrite(GREEN_PIN,0);
-   OCR1B=0;
-  
+  digitalWrite(RED_PIN, 0);
+  digitalWrite(GREEN_PIN, 0);
+  OCR1B = 0;
 }
 
-void set_color(int r, int g, int b){
-  
+void set_color(int r, int g, int b) {
+
   currentR = r;
   currentG = g;
   currentB = b;
 
-  g = g/4;
-  b = b/4;
+  g = g / 4;
+  b = b / 4;
 
   analogWrite(RED_PIN, r);
   analogWrite(GREEN_PIN, g);
   OCR1B = b;
- 
-  
+
+
+  // sprintf(buf, "set_color: %d %d %d, %d %d %d", currentR, currentG, currentB, r, g, b);
+  // mySerial.println(buf);
 }
 
-void set_color(uint32_t  hexColor) {
+void set_color(uint32_t hexColor) {
   int r = (hexColor >> 16) & 0xFF;
   int g = (hexColor >> 8) & 0xFF;
   int b = hexColor & 0xFF;
@@ -106,11 +56,19 @@ void set_color(uint32_t  hexColor) {
 
 
 uint32_t getCurrentColor() {
-  return (currentR << 16) | (currentG << 8) | currentB;
+
+  uint32_t toReturn = ((uint32_t)0x0 << 24) | ((uint32_t)currentR << 16) | ((uint32_t)currentG << 8) | (uint32_t)currentB;
+  sprintf(buf, "getCurrentColor: 0x%08lx %d %d %d", toReturn, currentR, currentG, currentB);
+  mySerial.println(buf);
+  return toReturn;
 }
 
 
 void fade_to_color(uint32_t fromHexColor, uint32_t toHexColor, uint32_t howManySeconds) {
+
+  if (fromHexColor == toHexColor)
+    return;
+
   int fromR = (fromHexColor >> 16) & 0xFF;
   int fromG = (fromHexColor >> 8) & 0xFF;
   int fromB = fromHexColor & 0xFF;
@@ -120,37 +78,42 @@ void fade_to_color(uint32_t fromHexColor, uint32_t toHexColor, uint32_t howManyS
   int toB = toHexColor & 0xFF;
 
   int steps = howManySeconds * 1000 / 20;
-  for (int i = 0; i <= steps; i++) {
+  uint8_t curR = fromR;
+  uint8_t curG = fromG;
+  uint8_t curB = fromB;
+  for (int i = 0; (i <= steps); i++) {
     float fraction = (float)i / steps;
-    int curR = fromR + (toR - fromR) * fraction;
-    int curG = fromG + (toG - fromG) * fraction;
-    int curB = fromB + (toB - fromB) * fraction;
+    curR = fromR + (toR - fromR) * fraction;
+    curG = fromG + (toG - fromG) * fraction;
+    curB = fromB + (toB - fromB) * fraction;
+    // sprintf(buf, "iter:  %d %d %d", curR, curG, curB);
+    // mySerial.println(buf);
     set_color(curR, curG, curB);
     delay(20);
   }
 }
 
 uint32_t colorForTemperature(int temp) {
-  if (temp <= 17) return 0x0000FF; // Deep Blue
-  if (temp >= 18 && temp <= 20) return 0x00FFFF; // Light Blue
-  if (temp >= 21 && temp <= 22) return 0x00FFFF; // Cyan
-  if (temp >= 23 && temp <= 24) return 0x00FF00; // Green
-  if (temp == 25) return 0x7FFF00; // Light Green
-  if (temp == 26) return 0x7FFF7F; // Green with Orange
-  if (temp == 27) return 0xFF7F00; // Orange
-  if (temp == 28) return 0xFFFF00; // Yellow
-  if (temp == 29) return 0xFFFF7F; // Yellow-Red
-  if (temp >= 30 && temp <= 31) return 0xFF0000; // Red
-  if (temp == 32) return 0xFF007F; // Red-Purple
-  if (temp >= 33) return 0x7F007F; // Purple
-  return 0x000000; // Default Black
+  if (temp <= 17) return 0x0000FF;                // Deep Blue
+  if (temp >= 18 && temp <= 20) return 0x00FFFF;  // Light Blue
+  if (temp >= 21 && temp <= 22) return 0x00FFFF;  // Cyan
+  if (temp >= 23 && temp <= 24) return 0x00FF00;  // Green
+  if (temp == 25) return 0x7FFF00;                // Light Green
+  if (temp == 26) return 0x7FFF7F;                // Green with Orange
+  if (temp == 27) return 0xFF7F00;                // Orange
+  if (temp == 28) return 0xFFFF00;                // Yellow
+  if (temp == 29) return 0xFFFF7F;                // Yellow-Red
+  if (temp >= 30 && temp <= 31) return 0xFF0000;  // Red
+  if (temp == 32) return 0xFF007F;                // Red-Purple
+  if (temp >= 33) return 0x7F007F;                // Purple
+  return 0x000000;                                 // Default Black
 }
 
 void fade_based_on_temperature(int temp, uint32_t howManySeconds) {
-  uint32_t currentColor = getCurrentColor(); // Assume this function returns the current color
+  uint32_t currentColor = getCurrentColor();
   uint32_t targetColor = colorForTemperature(temp);
-  char buf[20];
-  sprintf(buf, "Color: 0x%08lx", (unsigned long) targetColor);
+
+  sprintf(buf, "From: 0x%08lx To: 0x%08lx", (unsigned long)currentColor, (unsigned long)targetColor);
   mySerial.println(buf);
   fade_to_color(currentColor, targetColor, howManySeconds);
 }
@@ -180,53 +143,24 @@ void setup() {
   TCCR0B |= (1 << CS00);
 
   // Timer/Counter1 for Blue (PB4)
-  GTCCR |= (1 << PWM1B) | (1 << COM1B1); // Enable PWM and set compare output mode
+  GTCCR |= (1 << PWM1B) | (1 << COM1B1);  // Enable PWM and set compare output mode
 
   reset_color();
 
   mySerial.begin(9600);
   sensor.begin();
-  // fade_in(0x111111);
-  // fade_out();
-  // fade_in(0x333333);
-  // fade_out();
-  // fade_in(0x666666);
-  // fade_out();
-  
 }
 
 void loop() {
 
-  // int a = getChipTemperatureCelsius();
-   mySerial.println("hello");
-  // mySerial.println(a);
-  //set_color(0xFFFF00);
-  //set_color(0xFF7F00);
-  //set_color(0xFF0000);
   sensor.requestTemperatures();
   double temp = sensor.getTempCByIndex(0);
 
   mySerial.print("Temp: ");
   mySerial.println(temp);
 
+  fade_based_on_temperature((int)temp, 5);
 
-  //fade_based_on_temperature((int)temp, 5);
-  
+  delay(1000);
 
-  delay(1000); 
- 
-  //Fade in to red
-  //fade_in(0x110000);
-
-  // // Fade from red to green
-  // fade_to_color(0x110000, 0x001100, 2);
-  // delay(1000);
-
-  // // Fade from green to blue
-  // fade_to_color(0x001100, 0x000011, 2);
-  // delay(1000);
-
-  // // Fade out
-  // fade_out();
-  delay(500);
 }
